f5636320f0b3f65d6141bff1b5539737eb1bc997
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2024-01-27 03:26:14 +0530

    Footer

--- food_odering_app/client/src/view/common/Footer/Footer.tsx
+++ food_odering_app/client/src/view/common/Footer/Footer.tsx
@@ -3,8 +3,9 @@ import React, {Component} from 'react';
 export class Footer extends Component {
     render() {
         return (
-            <div>
-
+            <div className='bg-black w-screen h-52 mt-5 flex flex-col justify-center items-center'>
+                <h1 className='font-extrabold text-2xl text-white'>Swiggy</h1>
+                <h1 className='text-white'>© 2023 Bundl Technologies Pvt. Ltd</h1>
             </div>
         );
     }
--- food_odering_app/client/src/view/pages/Home/Home.tsx
+++ food_odering_app/client/src/view/pages/Home/Home.tsx
@@ -1,25 +1,26 @@
 import React, {Component} from "react";
 import {Navbar} from "../../common/Navbar/Navbar";
+import {Footer} from "../../common/Footer/Footer";
 
 export class Home extends Component{
 
     render() {
         return (
             <>
-                <ToastContainer autoClose={3000}/>
+                {/*<ToastContainer autoClose={3000}/>*/}
                 <div>
-                    <Navbar restaurantsList={restarantsList}/>
+                    {/*<Navbar restaurantsList={restarantsList}/>*/}
                     <div className='ml-44 w-8/12'>
                         <h1 className='mt-7 font-bold text-2xl'>Best offers for you</h1>
-                        <Offers/>
+                        {/*<Offers/>*/}
                         <h1 className='mt-7 font-bold text-2xl'>What's on your mind?</h1>
-                        <Menu setMenu={setMenu}/>
-                        <h1 className='mt-7 font-bold text-2xl'>Top restaurant chains in {restarantsList ? restarantsList[0]?.address_obj?.city : "Location"}</h1>
-                        <TopRated restaurantsList={restarantsList}/>
+                        {/*<Menu setMenu={setMenu}/>*/}
+                        {/*<h1 className='mt-7 font-bold text-2xl'>Top restaurant chains in {restarantsList ? restarantsList[0]?.address_obj?.city : "Location"}</h1>*/}
+                        {/*<TopRated restaurantsList={restarantsList}/>*/}
                     </div>
                     <div className='ml-44'>
-                        <h1 className='mt-7 font-bold text-2xl'>Restaurants with online food delivery in {restarantsList ? restarantsList[0]?.address_obj?.city : "Location"}</h1>
-                        <Restaurants menu={menu} restaurantsList={restarantsList}/>
+                        {/*<h1 className='mt-7 font-bold text-2xl'>Restaurants with online food delivery in {restarantsList ? restarantsList[0]?.address_obj?.city : "Location"}</h1>*/}
+                        {/*<Restaurants menu={menu} restaurantsList={restarantsList}/>*/}
                     </div>
                     <Footer/>
                 </div>
