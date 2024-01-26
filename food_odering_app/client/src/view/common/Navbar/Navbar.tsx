05fb255510239f78a7d0718e328073bf029ae307
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2024-01-27 02:40:13 +0530

    Navbar and Main Content Root

--- food_odering_app/client/src/view/common/Navbar/Navbar.tsx
+++ food_odering_app/client/src/view/common/Navbar/Navbar.tsx
@@ -3,9 +3,33 @@ import React, {Component} from 'react';
 export class Navbar extends Component {
     render() {
         return (
-            <div>
-
-            </div>
+            <>
+                <ToastContainer autoClose={3000}/>
+                <div className='flex items-center p-3 shadow-lg h-20 w-screen'>
+                    <img src={logo} className='w-14 h-14'/>
+                    <h1 className='font-bold text-sm underline ml-5'>Other</h1>
+                    {props?.restaurantsList ? <h1 className='text-sm ml-3'>{props?.restaurantsList[0]?.address_obj?.city}</h1>
+                        : <h1 className='text-sm ml-3'>Location</h1>}
+                    <img src={arrow} className='w-7 h-7'/>
+                    <div className='ml-56 flex items-center'>
+                        <Link to="/search" state={{data:props?.restaurantsList}}>
+                            <div className='flex items-center cursor-pointer'>
+                                <img src={lens} className='w-4 h-4 ml-96'/>
+                                <h1 className='ml-3'>Search</h1>
+                            </div>
+                        </Link>
+                        <img src={profile} className='w-7 h-7 ml-16'/>
+                        <div className='flex items-center cursor-pointer' onClick={()=> setLoginModal(true)}>
+                            <h1 className='ml-3'>Sign In</h1>
+                            <div onClick={logout} className='flex items-center cursor-pointer'>
+                                <img src={out} className='w-5 h-5 ml-16'/>
+                                <h1 className='ml-3'>Logout</h1>
+                            </div>
+                        </div>
+                    </div>
+                    {loginModal && <Login setLoginModal={setLoginModal}/>}
+                </div>
+            </>
         );
     }
 }
--- food_odering_app/client/src/view/pages/Home/Home.tsx
+++ food_odering_app/client/src/view/pages/Home/Home.tsx
@@ -1,12 +1,29 @@
-import {Component} from "react";
+import React, {Component} from "react";
+import {Navbar} from "../../common/Navbar/Navbar";
 
 export class Home extends Component{
 
     render() {
         return (
-            <div>
-
-            </div>
+            <>
+                <ToastContainer autoClose={3000}/>
+                <div>
+                    <Navbar restaurantsList={restarantsList}/>
+                    <div className='ml-44 w-8/12'>
+                        <h1 className='mt-7 font-bold text-2xl'>Best offers for you</h1>
+                        <Offers/>
+                        <h1 className='mt-7 font-bold text-2xl'>What's on your mind?</h1>
+                        <Menu setMenu={setMenu}/>
+                        <h1 className='mt-7 font-bold text-2xl'>Top restaurant chains in {restarantsList ? restarantsList[0]?.address_obj?.city : "Location"}</h1>
+                        <TopRated restaurantsList={restarantsList}/>
+                    </div>
+                    <div className='ml-44'>
+                        <h1 className='mt-7 font-bold text-2xl'>Restaurants with online food delivery in {restarantsList ? restarantsList[0]?.address_obj?.city : "Location"}</h1>
+                        <Restaurants menu={menu} restaurantsList={restarantsList}/>
+                    </div>
+                    <Footer/>
+                </div>
+            </>
         );
     }
 }
