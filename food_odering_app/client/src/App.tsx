96856a39b91bf62baa8746252dffa37009a01dae
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2024-01-27 23:21:19 +0530

    Update tsx

--- food_odering_app/client/src/App.tsx
+++ food_odering_app/client/src/App.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {Home} from "./view/pages/Home/Home";
+import {Login} from "./view/pages/Login/Login";
 
 
 function App() {
   return (
       <BrowserRouter>
           <Routes>
-              <Route path="/*" Component={Home} ></Route>
+              <Route path="/*" Component={Login} ></Route>
           </Routes>
       </BrowserRouter>
   );
