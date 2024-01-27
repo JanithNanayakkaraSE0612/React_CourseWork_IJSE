07ac825e7ee8941c42ddc0e541eefb0e3f54be5a
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2024-01-27 13:48:54 +0530

    Login Form added

--- food_odering_app/client/src/App.tsx
+++ food_odering_app/client/src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {Home} from "./view/pages/Home/Home";
+
 
 function App() {
   return (
