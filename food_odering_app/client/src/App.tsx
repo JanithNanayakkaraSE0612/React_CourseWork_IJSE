8dfe22e57e4ce501d6b2d61c5f0e9d137a1719df
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2024-01-30 20:31:19 +0530

    Sign up Form

--- food_odering_app/client/src/App.tsx
+++ food_odering_app/client/src/App.tsx
@@ -3,13 +3,14 @@ import './App.css';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {Home} from "./view/pages/Home/Home";
 import {Login} from "./view/pages/Login/Login";
+import {MainContent} from "./view/common/MainContent/MainContent";
 
 
 function App() {
   return (
       <BrowserRouter>
           <Routes>
-              <Route path="/*" Component={Login} ></Route>
+              <Route path="/*" Component={MainContent} ></Route>
           </Routes>
       </BrowserRouter>
   );
