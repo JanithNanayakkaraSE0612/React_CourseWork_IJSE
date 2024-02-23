83b842667cca9659352ef41a8701663cd4b20473
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2024-02-23 22:59:58 +0530

    material ui error debugging navbarLink

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,20 +1,22 @@
-import { AppBar,Tab, Tabs, Toolbar, Typography } from '@mui/material'
+import { AppBar,Toolbar, Typography } from '@mui/material'
+import { Tab, Tabs } from '@material-ui/core';
 import React, { useState } from 'react'
 import LibraryBooksIcon from '@mui/icons-material/LibraryBooks';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
     const [value ,setValue] = useState();
   return (
     <div>
-        <AppBar position='sticky'>
+        <AppBar sx={{backgroundColor:"#232F3D"}} position='sticky'>
             <Toolbar>
                 <Typography>
                     <LibraryBooksIcon/>
                 </Typography>
-                <Tabs textColor='inherit' indicatorColor='secondary' value={value} onChange={(e,val)=>setValue(val)}>
-                    <Tab label='Add Product' />
-                    <Tab label='Books' />
-                    <Tab label='About Us' />
+                <Tabs sx={{ml:"auto"}} textColor='inherit' indicatorColor='secondary' value={value} onChange={(e,val)=>setValue(val)}>
+                    <Tab LinkComponent={NavLink} to="/add" label='Add Product' />
+                    <Tab LinkComponent={NavLink} to="/books" label='Books' />
+                    <Tab LinkComponent={NavLink} to="/about" label='About Us' />
                 </Tabs>
             </Toolbar>
         </AppBar>
