86dbb355988cfc226f98a0729cedb56f8d339f1e
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2024-02-25 10:36:25 +0530

    Update

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -13,9 +13,9 @@ const Header = () => {
                     <LibraryBooksIcon/>
                 </Typography>
                 <Tabs sx={{ml:"auto"}} textColor='inherit' indicatorColor='secondary' value={value} onChange={(e,val)=>setValue(val)}>
-                    <Tab LinkComponent={NavLink} to="/add" label='Add Product' />
-                    <Tab LinkComponent={NavLink} to="/books" label='Books' />
-                    <Tab LinkComponent={NavLink} to="/about" label='About Us' />
+                       <Tab LinkComponent={NavLink} to="/add" label="Add product" />
+                       <Tab LinkComponent={NavLink} to="/books" label="Books" />
+                       <Tab LinkComponent={NavLink} to="/about" label="About Us" />
                 </Tabs>
             </Toolbar>
         </AppBar>
