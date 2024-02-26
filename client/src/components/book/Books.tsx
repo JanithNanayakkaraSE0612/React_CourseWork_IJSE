fe9af1e887d3c376afb5388dafa76de92cf680b8
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2024-02-26 10:52:51 +0530

    app.js config

--- client/src/components/book/Books.tsx
+++ client/src/components/book/Books.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 const URL = "http://localhost:3000/books"
 import axios from 'axios'
 
@@ -6,6 +6,11 @@ const fetchHandler = async()=>{
     return await axios.get(URL).then((res)=>res.data)
 }
 const Books = () => {
+    const [books , setBooks] = useState();
+    useEffect(()=>{
+       fetchHandler().then((data)=>setBooks(data)) ;
+    });
+    console.log(books);
   return (
     <div>Books</div>
   )
